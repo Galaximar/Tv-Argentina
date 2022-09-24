0ed8eee2e588ee3fc96e8b7f0a2d928eae952be7
Galaximar <marcelogottardini@vaypol.com.ar>
2022-09-24 20:30:32 -0300

    fix loading

--- src/components/ScriptVideo/index.js
+++ src/components/ScriptVideo/index.js
@@ -10,56 +10,57 @@ export default function ScriptVideo({ videoChannel, scriptVideo }) {
   const { type, channel } = videoChannel;
   const [loading, setLoading] = useState(true);
   const setManualIframe = () => (
-    <iframe {...scriptVideo.options} onLoad={() => setLoading(false)} />
+    <iframe
+      style={{ display: loading ? "none" : "block" }}
+      {...scriptVideo.options}
+      onLoad={() => setLoading(false)}
+    />
   );
   useEffect(() => {
     setLoading(true);
   }, [videoChannel]);
   useEffect(() => {
-    if (document.getElementsByTagName("iframe")[0]) {
-      document.getElementsByTagName("iframe")[0].remove();
+    if (document.getElementsByTagName("iframe")[0] && scriptVideo.url) {
+      document.getElementsByTagName("iframe")[0]?.remove();
     }
     window.setTimeout(() => {
       switch (type) {
         case "twitch":
+          console.log("entra", document.getElementsByTagName("iframe")[0]);
           if (
             typeof Twitch !== "undefined" &&
             !document.getElementsByTagName("iframe")[0]
           ) {
             setLoading(false);
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
             new Twitch.Player("video", scriptVideo.options);
           }
           break;
         default:
           break;
       }
-    }, 8000);
+    }, 1000);
   }, [videoChannel]);
-
   return (
     <div className={styles.iframeContainer}>
       {scriptVideo.url ? (
         <>
           <Script src={scriptVideo.url} />
-          <div id="video" className={styles.videoContainer} />
+          <div
+            style={{ display: loading ? "none" : "block" }}
+            id="video"
+            className={styles.videoContainer}
+          />
         </>
       ) : (
         setManualIframe()
       )}
-      {console.log("loadiing ", loading)}
+
       {loading && (
-        <div
-          style={{
-            width: "80vw",
-            height: "50vw",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "30px",
-          }}
-          className={styles.loading}
-        >
+        <div className={styles.loading}>
           CARGANDO....
           <Circles
             height="80"
--- src/scriptVideos.js
+++ src/scriptVideos.js
@@ -12,7 +12,6 @@ export const SCRIPT_VIDEOS = {
     options: {
       width: "853",
       height: "480",
-      src: "https://www.youtube.com/embed/41YAYbmRVq4?autoplay=1&mute=1&loop=1&quality=high",
       frameBorder: "0",
       allow:
         "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
